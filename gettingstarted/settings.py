1b5540718a14981ffff4a52090b2ac4ce3eef807
fzapfack <fabricezapfack@gmail.com>
2017-03-17 18:09:07 +0100

    can't find loader.utils

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello',
-    'loader'
+    'loader',
 )
 
 MIDDLEWARE_CLASSES = (
--- loader/views.py
+++ loader/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .utils import doctoctoc
+
 
 # Create your views here.
+def load_data(request):
+    print('Loading data from xls')
+
+    return HttpResponse('Twitter listener started')
