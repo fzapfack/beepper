09199b91428c3dfbf53bdd9db1ac73db86a939b8
fzapfack <fabricezapfack@gmail.com>
2017-03-26 17:48:52 +0200

    clean pic

--- hello/utils/clean.py
+++ hello/utils/clean.py
@@ -2,14 +2,19 @@ import re
 
 
 def clean_tweet(txt, remove_hashtags=False):
+    # remove pic links
+    clean = re.sub(r'pic.twitter\S+', '', txt)
+    # clean1=txt.split('pic.twitter')[0]
     # Remove @
-    clean1=txt.split('pic.twitter')[0]
-    clean = re.sub('@\w+', '', clean1)
+    clean = re.sub('@\w+', '', clean)
+    # Remove #
     if remove_hashtags:
         clean = re.sub('#\w+', '', clean)
         good = True
     else:
         clean = re.sub('#[dD][oO][cC][tT][oO][cC]\w+', '', clean)
         a = re.findall('#\w+', txt)
-        good = len(a)>0
+        good = len(a) > 0
     return clean, good
+
+    return True
